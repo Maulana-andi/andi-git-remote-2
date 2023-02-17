34291c979640b137f5b8e7a0ba4a2be54ff12c74
Maulana-andi <andi_maulana9@ymail.com>
2023-02-17 10:15:29 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel mollitia nostrum, impedit sapiente magni iure modi repellendus dolore voluptatum illo enim ullam repellat doloribus culpa placeat. Culpa tenetur maxime doloremque!</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 </html>
